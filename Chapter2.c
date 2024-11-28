029f7e1b4cd9d3d9e5e6750c6776c6439305f688
Franciscofmv <francisco.moyet@outlook.com>
2024-11-28 12:21:07 -0500

    First Push

---
+++ Chapter2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    // int hour, minute, second;
+    // hour = 1;
+    // minute = 2;
+    // second = 4;
+
+    // printf("The time is %i:%i:%i\n", hour, minute, second);
+    // printf("%c\n", '5');
+    char letter = 'd';
+    printf("%c", letter*9);
+    return(EXIT_SUCCESS);
+}
---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hello World!\n");
+    printf("How are you?\n");
+    //This is a comment
+    return(EXIT_SUCCESS);
+}
